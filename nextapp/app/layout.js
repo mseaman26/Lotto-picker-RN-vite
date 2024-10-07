662a88f9738b32fe762083d50b9104623aaf356a
michael seaman <mseaman26@gmail.com>
2024-10-07 00:14:41 -0700

    path to favicon

--- nextapp/app/layout.js
+++ nextapp/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/app/favicon.ico" />
         {/* Other head elements */}
       </head>
       <body>{children}</body>
